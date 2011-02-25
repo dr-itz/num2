d03c82be69ceeeddf92f128d1a2a260d8b7841f4
Daniel Ritz <daniel.ritz@gmx.ch>
2011-02-26 00:23:23 +0100

    u012: rename file, add printf, bank format
    Looks a bit nicer, showing the real numbers
    
    Signed-off-by: Daniel Ritz <daniel.ritz@gmx.ch>

--- u01/u012gehaltsstufen.m
+++ u01/u012gehaltsstufen.m
@@ -1,5 +1,5 @@
 %Aufgabe u012
-%Von 5 höheren Gehaltsstufen verdient jede nächsthöhere Stufe 20% mehr als die darunterliegende
+%Von 5 h?heren Gehaltsstufen verdient jede n?chsth?here Stufe 20% mehr als die darunterliegende
 %und dazu 20'000 Fr mehr pro Jahr. Gesamtsumme = 1'000'000
 % -: gs1 = gs1
 % 1: gs2 = 1.2*gs1 + 20
@@ -10,6 +10,9 @@
 % A * x = b
 
 A=[-1.2 1 0 0 0; 0 -1.2 1 0 0; 0 0 -1.2 1 0; 0 0 0 -1.2 1; 1 1 1 1 1]
-b=[20 20 20 20 1000 ]'
+b=1000*[20 20 20 20 1000 ]'
 
+format bank
+fprintf('Gehaltsstufen');
 x = A\b
+format short
