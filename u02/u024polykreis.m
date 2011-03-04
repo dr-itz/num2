d95e0e02ad9f1701003a3a54ed06e4674861b7e1
Daniel Ritz <daniel.ritz@gmx.ch>
2011-03-04 11:20:47 +0100

    u024: polykreis
    Signed-off-by: Daniel Ritz <daniel.ritz@gmx.ch>

---
+++ u02/u024polykreis.m
@@ -0,0 +1,30 @@
+function [ r ] = u024polykreis( n, s )
+%U024POLYKREIS Vieleck mit Umkreis
+%   n: Anzahl Ecken, s: Seitenlaenge
+% gibt den Radius zurueck
+
+a = 360 / n;
+r = s / (2*sind(a/2));
+
+% kreis
+w = 0:2:360;
+x = sind(w)*r;
+y = cosd(w)*r;
+
+clf;
+plot(x,y);
+
+hold on
+axis equal;
+
+% n-Eck
+w = 0:360/n:360;
+x = cosd(w)*r;
+y = sind(w)*r;
+
+plot(x,y);
+figure(1);
+hold off;
+
+end
+
