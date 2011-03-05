0c702109ecc0619a63a74f727ff3c4bf49eeac08
Daniel Ritz <daniel.ritz@gmx.ch>
2011-03-05 18:13:44 +0100

    u021: aufteilung in berechnung und beispiel
    beispiel mit fest vorgegebener "index-matrix" statt rand()
    
    Signed-off-by: Daniel Ritz <daniel.ritz@gmx.ch>

--- u02/u021permutation.m
+++ u02/u021permutation.m
@@ -1,15 +1,13 @@
-function [ T1, T2, P1, P2 ] = u021( v )
+function [ T1, T2 ] = u021permutation( v )
 % U021 Permutationsmatrizen aus Permutationsvektoren
 % Input Parameter v: Permutationsvektor mit beliebiger Reihenfolge
 %                    der Zahlen von 1 bis n
-%                    Bsp: v = [3 5 1 2 4] 
+%                    Bsp: v = [3 5 1 2 4]'
 %
-% Output Parameter T1, T2, P1, P2: 2 quadratische n x n Turmmatrizen, wobei
-% T1 fuer die Zeilenpermutation P1 bei Multiplikation von links und 
-% T2 fuer die Spaltenpermutation P2 bei Multiplikation von rechts verwendet
+% Output Parameter T1, T2: 2 quadratische n x n Turmmatrizen, wobei
+% T1 fuer die Zeilenpermutation bei Multiplikation von links und 
+% T2 fuer die Spaltenpermutation bei Multiplikation von rechts verwendet
 % werden
-
-clc;
 
 [n,m] = size(v);
 T1 = zeros(n);
@@ -19,15 +17,5 @@ for i = 1:n
     T1(i,v(i)) = 1;
     T2(v(i),i) = 1;
 end
-
-T1
-T2
-M = rand(n)
-
-fprintf('Kontrolle Zeilenpermutation bei Links-Multiplikation P1 = T1 * M:')
-P1 = T1*M
-
-fprintf('Kontrolle Spaltenpermutation bei Rechts-Multiplikation P2 = M * T2:')
-P2 = M*T2
 
 end
---
+++ u02/u021permutationBeispiel.m
@@ -0,0 +1,14 @@
+% Beispiel fuer Zeilen und Spaltenpermutation
+
+v = [3 5 1 2 4]'
+
+[ T1, T2 ] = u021permutation( v )
+
+M = [11 12 13 14 15; 21 22 23 24 25; 31 32 33 34 35; ...
+    41 42 43 44 45; 51 52 53 54 55]
+
+fprintf('Kontrolle Zeilenpermutation bei Links-Multiplikation P1 = T1 * M:')
+P1 = T1*M
+
+fprintf('Kontrolle Spaltenpermutation bei Rechts-Multiplikation P2 = M * T2:')
+P2 = M*T2
