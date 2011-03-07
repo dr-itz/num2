7f0d61fa93f8aa992910f62b296a661b43d1591d
ckaufman <ckaufman@hsz-t.ch>
2011-03-07 21:51:22 +0100

    u021: print P1+P2 on new line

--- u02/u021permutationBeispiel.m
+++ u02/u021permutationBeispiel.m
@@ -1,4 +1,4 @@
-% Beispiel fuer Zeilen und Spaltenpermutation
+% Beispiel fuer Zeilen- und Spaltenpermutation
 
 v = [3 5 1 2 4]'
 
@@ -7,8 +7,8 @@ v = [3 5 1 2 4]'
 M = [11 12 13 14 15; 21 22 23 24 25; 31 32 33 34 35; ...
     41 42 43 44 45; 51 52 53 54 55]
 
-fprintf('Kontrolle Zeilenpermutation bei Links-Multiplikation P1 = T1 * M:')
+fprintf('Kontrolle Zeilenpermutation bei Links-Multiplikation P1 = T1 * M:\n')
 P1 = T1*M
 
-fprintf('Kontrolle Spaltenpermutation bei Rechts-Multiplikation P2 = M * T2:')
+fprintf('Kontrolle Spaltenpermutation bei Rechts-Multiplikation P2 = M * T2:\n')
 P2 = M*T2
