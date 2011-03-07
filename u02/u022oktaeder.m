69f3441acb27fb64c932cd78e4c99318dfcf95d6
Sandy Freihofer <sandra.freihofer@gmail.com>
2011-03-07 14:58:45 +0100

    u022: oktaeder update
    Signed-off-by: Sandy Freihofer <sandra.freihofer@gmail.com>

--- u02/u022oktaeder.m
+++ u02/u022oktaeder.m
@@ -1,64 +1,43 @@
 %U022OKTAEDER Oktaeder - doppelte vierseitige Pyramide
-%   Zeichnet ein Oktaeder mit der Kantenlaenge 10
+%   Zeichnet ein Oktaeder mit der Kantenlaenge 10 und den
+%   6 Ecken bei Nord, Ost, Sued, West, Top, Bottom, auf den
+%   Koordinatenachsen +y, +x, -y, -x, +z, -z
 
 % Alles bereinigen
 clear;clc;clf;
 
+% Hoehe hp der Pyramide: hp = sqrt((s^2 * 3)/4 - s^2/4)
+% => hp = s / sqrt(2)
+
+% x bzw. y Abstand vom Zentrum, da 45 Grad gedreht damit Ecken bei
+% Nord, Ost, Sued, West liegen
+a = 10 / sqrt(2);
+
+% Punkte
+%     x  y  z
+A = [-a  0  0]'; % Westen
+B = [0  -a  0]'; % Sueden
+C = [ a  0  0]'; % Osten
+D = [0   a  0]'; % Norden
+E = [0   0  a]'; % Top
+F = [0   0 -a]'; % Bottom
+
+% Vektor-Zusammenstellung des Oktaeders
+O = [A B C D A E B E C E D F A F B F C]
+
+% 3D Plot mit labels
+%       x       y       z
+plot3(O(1,:), O(2,:), O(3,:),'r-');
+d = 1;
+text(-a-d, 0, 0,'A');
+text(0, -a-d, 0,'B');
+text(a+d, 0, 0,'C');
+text(0, a+d, D(3),'D');
+text(0, 0, a+d,'E');
+text(0, 0, -a-d,'F');
+axis equal
+
+
 % Figur im Vordergrund darstellen
 figure(1);
 
-% Eckpunkte A,B,C,D,TOP,BOT des Oktaeders
-% Spalte 1      2       3
-A =     [10     10      0]';
-B =     [-10    10      0]';
-C =     [-10    -10     0]';
-D =     [10     -10     0]';
-TOP =   [0      0       10*sqrt(2)]';
-BOT =   [0      0       10*-sqrt(2)]';
-
-P = [A B C D TOP BOT];
-  
-% Indexvektor
-idx = [1 2 5];
-fill3(P(1,idx), P(2,idx), P(3,idx), 'y');
-hold on
-
-% Zusammenstellung der gelben Pyramide
-% Indexvektor
-idx = [2 3 5];
-fill3(P(1,idx), P(2,idx), P(3,idx), 'y');
-
-idx = [3 4 5] ;
-fill3(P(1,idx), P(2,idx), P(3,idx), 'y');
-
-idx = [4 1 5];
-fill3(P(1,idx), P(2,idx), P(3,idx), 'y');
-
-
-% Zusammenstellung der gruenen Pyramide
-% Indexvektor
-idx = [1 2 6];
-fill3(P(1,idx), P(2,idx), P(3,idx), 'g');
-
-% Indexvektor
-idx = [2 3 6];
-fill3(P(1,idx), P(2,idx), P(3,idx), 'g');
-
-idx = [3 4 6];
-fill3(P(1,idx), P(2,idx), P(3,idx), 'g');
-
-idx = [4 1 6];
-fill3(P(1,idx), P(2,idx), P(3,idx), 'g');
-
-% Transparenz der gezeichneten Ebenen aus [0,1]
-alpha(0.7);
-
-% Eckpunkte des Oktaeders beschriften
-text(-11.5, -10, 0, 'A')
-text(8.7, -10, -1.5, 'B')
-text(11, 10, 0, 'C')
-text(-9.7, 10, -2, 'D')
-text(-1, 0, 15.5, 'TOP')
-text(-1, 0, -15.5, 'BOT')
-
-axis equal
