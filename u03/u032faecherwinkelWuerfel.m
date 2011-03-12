8815fa98af647918a1531756cb8fed7c9bc39d0c
Daniel Ritz <daniel.ritz@gmx.ch>
2011-03-12 19:16:15 +0100

    u032faecherwinkel.m
    - acosd() statt acos(), damit die Angaben in Grad sind
    - Beispiel der Ausgaben in u32beispiel.m
    
    Gemäss den Notizen
    % 2. eingabe: [w1, w2, w3] = u032faecherwinkelWuerfel(1)
    % arctan(1/sqrt(2))  % ergibt w2: schnitt durch würfel
    => in MATLAB: atand(1/sqrt(2)) = 35.26. Dies stimmt mit der Ausagbe überein
    
    Signed-off-by: Daniel Ritz <daniel.ritz@gmx.ch>

--- u03/u032faecherwinkelWuerfel.m
+++ u03/u032faecherwinkelWuerfel.m
@@ -5,14 +5,15 @@ function [wl, wm, wr] = u032faecherwinkelWuerfel(p)
 %                    Bsp.: 0 = Ebene ABCD, 1 = Ebene EFGH
 % Output Parameter wl, wm, wr: Winkel links, Mitte, rechts
 
+s = 5;                    % Seitenlaenge, spielt keine Rolle
 a = [0 0 0]';
-b = [0 4 0]';
-c = [4 4 0]';
-d = [4 0 0]';
-e = [0 0 4]';
-f = [0 4 4]';
-g = [4 4 4]';
-h = [4 0 4]';
+b = [0 s 0]';
+c = [s s 0]';
+d = [s 0 0]';
+e = [0 0 s]';
+f = [0 s s]';
+g = [s s s]';
+h = [s 0 s]';
 
 bv = c - a;
 vl = d * (1-p) + p*h;     %Vektor links
@@ -23,11 +24,8 @@ wl = angofvect(vl, bv);   %Winkel links
 wm = angofvect(vm, bv);   %Winkel Mitte
 wr = angofvect(vr, bv);   %Winkel rechts
 
+end
+
 function w = angofvect(u,v)
-
-w = acos(u'*v/(norm(u)*norm(v)));
-
-% 1. eingabe: [w1, w2, w3] = u032faecherwinkelWuerfel(0)
-% w3 * 180/pi
-% 2. eingabe: [w1, w2, w3] = u032faecherwinkelWuerfel(1)
-% arctan(1/sqrt(2))  % ergibt w2: schnitt durch würfel
+w = acosd(u'*v/(norm(u)*norm(v)));
+end
---
+++ u03/u32beispiel.m
@@ -0,0 +1,8 @@
+% zeigt Ausgabe der Funktion u032faecherwinkelWuerfel
+format compact
+format bank
+clear;clc;
+fprintf('u032faecherwinkelWuerfel mit p = 0, alle Winkel in Grad:\n');
+[wl, wm, wr] = u032faecherwinkelWuerfel(0)
+fprintf('u032faecherwinkelWuerfel mit p = 1, alle Winkel in Grad:\n');
+[wl, wm, wr] = u032faecherwinkelWuerfel(1)
