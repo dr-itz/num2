27e0feefaafbdbc1da81ff060e46e1b96faf9551
patrikkeller <patrik.keller@gmail.com>
2011-03-12 20:16:03 +0100

    u031 pake -2

--- u03/u031.m
+++ u03/u031.m
@@ -1,18 +1,19 @@
 function [ v ] = u031( n )
-%U031 Summary of this function goes here
+%U031 Vektor mit n-2 Werten welche den Winkeln zwischen den Seiten entsprechen
+% hier zwischen E1-E2 und E(k-1)-Ek mit k=3 ..n
 %   Detailed explanation goes here
 if n<3
     error('zu kleiner Wert')
 end
 alpha=360/n;
-x=3;
+k=3;
 beta=0;
 n=n-1;
 v=[];
 beta=180-alpha;
 v=[beta];           %erster Winkel
 
-for x = x:n
+for k = k:n
     beta=mod(beta-alpha,360);
     v=[v beta];
 end
