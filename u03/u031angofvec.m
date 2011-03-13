badbc2d71b7a323726bd166a0f91491900401904
patrikkeller <patrik.keller@gmail.com>
2011-03-13 23:07:58 +0100

    u031 als Faecherwinkel

---
+++ u03/u031angofvec.m
@@ -0,0 +1,7 @@
+function [ w ] = angofvec( u,v )
+%ANGOFVEC Summary of this function goes here
+%   Detailed explanation goes here
+    % Seite 184
+    cow=u'*v/(norm(u)*norm(v)); 
+    w=acosd(cow);
+end
--- u03/u031polygon.m
+++ u03/u031polygon.m
@@ -1,19 +1,33 @@
-%pk
-% Anzahl Ecken = n
-n=6
-alpha= (2*pi)/n
-r=1
-w=[0:.01:(2*pi)]
-y=r*cos(w)
-x=r*sin(w)
-%plot(x,y)
-%hold on
-AB= 2*r*sin((2*pi/n)/2)
-U=2*pi*r
-wstart=alpha
-wvec = wstart + (0:n)*(2*pi/n)
-r=[AB AB AB AB AB AB ]
-polar(wvec, r)
-%tbc
+function [W] = poly2(n)
+
+E1=[0 0]';
+b=[3 0]';
+alpha=(360/n);
+k=1;
+E2=E1+b;
+M=[E1 E2]
+alt=E2
+for k =k:(n-2)
+    k*alpha
+    DW=[cosd((k)*alpha) -sind((k)*alpha); sind((k)*alpha) cosd((k)*alpha)];
+    v=DW*b
+    neu=alt+v;
+    M=[M neu];
+    alt=neu
+end
+M=[M E1]                % Linie zurück zum Anfangspunkt, zum Zeichnen!
 
 
+% nun die Winkel
+W=[] ;                   % Resultatvektor
+Basis=M(:,2)-M(:,1);     % Basis
+k=2;
+for k=k:(n-1)
+    vek=M(:,k+1)-M(:,1);           % der Fächervektor beginnt immer bei E1 = M(:,1)
+    beta = u031angofvec(Basis,vek);   
+    W=[W beta];
+end
+
+% nun noch das Polynom zeichnen
+ clf
+ plot(M(1,:),M(2,:))
