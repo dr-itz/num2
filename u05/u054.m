12a229475d1b84f7a34d2d1ba7b009038add9b03
patrikkeller <patrik.keller@gmail.com>
2011-03-27 09:15:54 +0200

    u054 v3

--- u05/u054.m
+++ u05/u054.m
@@ -1,22 +1,22 @@
+% u054
 % eckig komplexe Spirale, by Patrik Keller
 % gesucht min p1 wenn imag(z^p1) negativ und wann wieder positiv
 z1=1+i*0.2;
 
 % suchen bis Imaginärteil kleiner 0
-disp('Suchen bis kleiner 0')
-p1=1
-while imag(z^p1)>=0
+disp('Potenz suchen, bis Imaginärteil von 1+i*0.2^p kleiner 0:')
+p1=1;
+while imag(z1^p1)>=0
     p1=p1+1;
 end
-p1
-z^p1
+disp('kleinste Potenz, so dass Imaginärteil kleiner 0',p1)
+z1^p1;
 
 % jetzt weitersuchen bis wieder >0
-disp('weitersuchen bis wieder >0')
-p2=p1
-while imag(z^p2)<0
+p2=p1;
+while imag(z1^p2)<0
     p2=p2+1;
 end
-p2 
-z^p2
+disp('nächst grössere Potenz bis Imaginärteil wieder positiv',p2)
+z1^p2;
 
