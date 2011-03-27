889fb7a62a8e6cf77ec2656d6150423ca602de61
patrikkeller <patrik.keller@gmail.com>
2011-03-27 09:01:16 +0200

    u054 v2

--- u05/u054.m
+++ u05/u054.m
@@ -1,7 +1,9 @@
-% gesucht min p1 wenn imag(z^p1) wird negativ
+% eckig komplexe Spirale, by Patrik Keller
+% gesucht min p1 wenn imag(z^p1) negativ und wann wieder positiv
 z1=1+i*0.2;
 
 % suchen bis Imaginärteil kleiner 0
+disp('Suchen bis kleiner 0')
 p1=1
 while imag(z^p1)>=0
     p1=p1+1;
@@ -10,9 +12,11 @@ p1
 z^p1
 
 % jetzt weitersuchen bis wieder >0
+disp('weitersuchen bis wieder >0')
 p2=p1
 while imag(z^p2)<0
     p2=p2+1;
 end
 p2 
 z^p2
+
