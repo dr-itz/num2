3894e2e60f650a57654df0a35882bf7ff3997881
ckaufman <ckaufman@hsz-t.ch>
2011-03-28 21:18:28 +0200

    u054: disp changed, only 1 param possible

--- u05/u054.m
+++ u05/u054.m
@@ -4,12 +4,12 @@
 z1=1+i*0.2;
 
 % suchen bis Imaginärteil kleiner 0
-disp('Potenz suchen, bis Imaginärteil von 1+i*0.2^p kleiner 0:')
 p1=1;
 while imag(z1^p1)>=0
     p1=p1+1;
 end
-disp('kleinste Potenz, so dass Imaginärteil kleiner 0',p1)
+disp('kleinste Potenz, so dass Imaginärteil kleiner 0')
+p1
 z1^p1;
 
 % jetzt weitersuchen bis wieder >0
@@ -17,6 +17,7 @@ p2=p1;
 while imag(z1^p2)<0
     p2=p2+1;
 end
-disp('nächst grössere Potenz bis Imaginärteil wieder positiv',p2)
+disp('nächst grössere Potenz bis Imaginärteil wieder positiv')
+p2
 z1^p2;
 
