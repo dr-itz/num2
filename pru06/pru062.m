2af8fb18be5d0f035ce6a69c3cd8a569d5fc6dea
Daniel Ritz <daniel.ritz@gmx.ch>
2011-03-29 15:13:02 +0200

    pru06: Kommentare...
    Signed-off-by: Daniel Ritz <daniel.ritz@gmx.ch>

--- pru06/pru062.m
+++ pru06/pru062.m
@@ -1,7 +1,12 @@
+% Ausangangswert
 z = 0.866 + 0.52i
 
+% 0-14: < 15
 for ii = 0:14,
+   % Potenzieren
    z2 = z^ii;
+   
+   % Real- und Imaginaerteil muss < 0 sein
    if real(z2) < 0 && imag(z2) < 0,
        fprintf('z^%d: ', ii);
        disp(z2);
