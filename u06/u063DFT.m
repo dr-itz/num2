90bdd42c6a0f64aa25a39abb72d52a0b6eb2c811
Sandy Freihofer <sandra.freihofer@gmail.com>
2011-04-29 17:21:40 +0200

    u063: Selbst programmierte DFT
    Signed-off-by: Sandy Freihofer <sandra.freihofer@gmail.com>

---
+++ u06/u063DFT.m
@@ -0,0 +1,27 @@
+% Selbst programmierte DFT: die ersten 9 cos-Koeffizienten
+
+f = [1 1 1 1 1 zeros(1,21) 1 1 1 1];
+T = length(f);
+a = zeros(1,9);
+
+for k = 0:8,
+    sc = 0;
+    for t = 0:T-1,
+        sc = sc + f(t+1) * cos(k*t*2*pi/T);
+    end
+    a(k+1) = 2 * sc / T;
+end
+
+a
+
+plot(f, 'r-');
+hold on;
+f2 = zeros(1,T);
+for t = 1:T,
+    for k = 1:9,
+        f2(t) = f2(t) + a(k)*cos(k*t*2*pi/T);
+    end
+end
+plot(f2, 'g-');
+hold off;
+figure(1)
