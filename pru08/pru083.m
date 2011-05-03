af55e029eb7441e2cb7659d642fb799ba8e09568
Daniel Ritz <daniel.ritz@gmx.ch>
2011-05-03 15:42:53 +0200

    pru083: fix graph
    Erster plot: "pf", nicht "pn".
    
    Signed-off-by: Daniel Ritz <daniel.ritz@gmx.ch>

--- pru08/pru083.m
+++ pru08/pru083.m
@@ -1,6 +1,6 @@
-% Bestimmen der Fehlergleichungsmatrix für den Fit einer Geraden 
+% Bestimmen der Fehlergleichungsmatrix fuer den Fit einer Geraden
 % an die Punkte x = -4 -2 0 2 4 und y = 3 2 2 1 1,
-% anschliessendes Lösen der Normalengleichungen. 
+% anschliessendes Loesen der Normalengleichungen.
 
 x = (-4:2:4)'
 y = [3 2 2 1 1]';
@@ -15,7 +15,7 @@ figure(1)
 plot(x,y,'+r'); 
 hold on; 
 axis([-5 5 0 5])
-plot([-5 5], [pn(2)-5*pn(1) pn(2)+5*pn(1)]);
+plot([-5 5], [pf(2)-5*pf(1) pf(2)+5*pf(1)]);
 
 % N = A'*A oder
 N = [sum(x.^2) sum(x); sum(x) 5]
