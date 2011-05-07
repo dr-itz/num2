597a00d588a61e22df05dede94206cb7d4d9902b
patrikkeller <patrik.keller@gmail.com>
2011-05-07 22:54:32 +0200

    u083 v2

--- u08/u083.m
+++ u08/u083.m
@@ -3,17 +3,18 @@
 % http://pi.physik.uni-bonn.de/~aprakt/AP-A4.pdf
 
 % Startwerte
-x=[-4:2:4];
-y= [2 6 5 5 7];
-wk=[2 4 3 2 1]';
+x=[-2:1:2];
+y= [-2 0 1 1.5 3];
+wk=[4 2 1 2 4]';
 
 % x,y mit Abweichung Plotten
 % Kreise bei x,y
 clf
-title('u083 Fit mit Gewicht')
-text(0,0.5,'blau = linear Fit; rot = lin. Fit mit Gewicht')
+gcf
 plot(x,y,'ro');
-axis([x(1)-1 x(length(x))+1 0 10]); hold on; grid on;
+axis([x(1)-1 x(length(x))+1 -4 4]); hold on; grid on;
+title('u083 Fit mit Gewicht')
+text(0,-2.5,'blau = linear Fit; rot = lin. Fit mit Gewicht')
 
   % Abweichungen = Linien bei xk, von yk-0.5(ws(k)) bis yk+0.5(ws(k))
   for k=1:size(x,2)
@@ -21,8 +22,21 @@ axis([x(1)-1 x(length(x))+1 0 10]); hold on; grid on;
     plot([x(k) x(k)], wkL,'r');
   end
 
+% n1 Normalengleichung
+disp('Normalengleichung')
+A=[sum(x.^2) sum(x); sum(x) length(x)]
+b=[0;0];
+for k=1:length(x)
+    b(1)=b(1)+( y(k)*x(k) );
+end
+b(2)=sum(y);
+b
+n1=A\b
+
+% n2 Normalengleichung mit Gewicht
 
 % g1 Geradenfit
+disp('Geradenfit')
 A=[x(1,:); ones(1,5)]'
 y=y'
 disp('A*g = y  // gX = g(1)*x + g(2)')
@@ -30,29 +44,40 @@ g1=A\y
 
 % g2 Geradenfit mit Gewicht
 % jede Zeile mit Gewichtungsfaktor aus wk multiplizieren
+disp('Geradenfit mit Gewicht')
 for k=1:size(x,2)
     A(k,:)=A(k,:)*wk(k);
     y(k)=y(k)*wk(k);
 end
-disp('wk =') 
+
 wk
 g2=A\y
 
 
-% Gerade 1 plotten
-X=[x(1)-1 x(length(x))+1];
-Y=[gerade(a(1),g1) gerade(a(2),g1)];
-plot(X,Y,'b');
 
-% Gerade 2 plotten
+
+% Gerade Normalengleichung plotten
 X=[x(1)-1 x(length(x))+1];
-Y=[gerade(a(1),g2) gerade(a(2),g2)];
+Y=[gerade(X(1),n1) gerade(X(2),n1)];
+plot(X,Y,'mo-')
+
+% Gerade Fehlergleichung 1 plotten
+X=[x(1)-1 x(length(x))+1];
+Y=[gerade(X(1),g1) gerade(X(2),g1)];
+plot(X,Y,'b--');
+
+% Gerade Fehlergleichung mit Fit 2 plotten
+X=[x(1)-1 x(length(x))+1];
+Y=[gerade(X(1),g2) gerade(X(2),g2)];
 plot(X,Y,'r')
-
 
 
 
 % innere Funktion für Gerade
 % x = x-Position
 % g1 = Paramter der Geraden y = x*g(1) + g(2)
-gerade = @(x,g) [x*g(1)+g(2)];
+gerade = @(x,g) x*g(1)+g(2);
+
+
+
+
