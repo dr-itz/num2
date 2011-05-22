f1355be682946a6d8075f96043899e90baa78ba6
Sandy Freihofer <sandra.freihofer@gmail.com>
2011-05-22 19:34:27 +0200

    u092: Nichtlineare Nebenbedingung und Abstandsfunktion
    To be continued...
    
    Signed-off-by: Sandy Freihofer <sandra.freihofer@gmail.com>

---
+++ u09/u092NebenbedAbstandsfkt.m
@@ -0,0 +1,20 @@
+% Nichtlineare Nebenbedingung und Abstandsfunktion, Buch S.295/60?27
+%   Stellen Sie das nichtlineare Gleichungssystem auf, welches das Optimum
+%   der Funktion F(x,y) = sqrt( x^2 + y^2 ) definiert, unter der Bedingung,
+%   dass y = 5 / x^3
+
+% Nebenbedingung in "0-Form" bringen:
+%   G(x,y) = y - 5 / x^3
+
+% Zielfunktion L(x,y,l) = sqrt(x^2 + y^2) + l * (y - 5 / x^3)
+
+
+% Gleichungssystem
+%
+% dL/dx = x / sqrt(x^2 + y^2) + l * 15/x^4  = 0
+%
+% dL/dy = y / sqrt(x^2 + y^2) + l           = 0
+%
+% dL/dl = y - 5/x^3                         = 0
+
+% => x = 1, y = 4, l = -4
