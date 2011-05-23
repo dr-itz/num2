12e9a1e0275922645a36d95e6ef9517ef1677b2c
Sandy Freihofer <sandra.freihofer@gmail.com>
2011-05-24 00:01:48 +0200

    u103: Kommentar hinzugefuegt
    Signed-off-by: Sandy Freihofer <sandra.freihofer@gmail.com>

--- u10/u103Kugelkoordinaten.m
+++ u10/u103Kugelkoordinaten.m
@@ -3,6 +3,7 @@
 % Vektor mit den Schaetz- bzw. Startwerten
 xvec = [4 0.4 0.6]';
 
+% Iteration von 1 bis 3, da 2-3 Verbesserungsschritte verlangt sind
 for i = 1:3
     xvec = xvec - kugjac(xvec) \ kugfct(xvec)
 end
